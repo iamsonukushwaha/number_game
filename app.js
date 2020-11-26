a00187b9ceac31cc10cb0efc4d5558a31b591d5b
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2020-11-26 14:51:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const plus = (num1, num2) => num1 + num2;
+const minus = (num1, num2) => num1 - num2;
+const product = (num1, num2) => num1 * num2;
+const division = (num1, num2) => num1 / num2;
+const pl = '+';
+const mi = '-';
+const pr = '*';
+const di = '/';
+
+module.exports.plus = plus;
+module.exports.minus = minus;
+module.exports.product = product;
+module.exports.division = division;
+module.exports.pl = pl;
+module.exports.mi = mi;
+module.exports.pr = pr;
+module.exports.di = di;
