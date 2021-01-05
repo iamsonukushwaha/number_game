88441eb707e337a037e50c783f737bb1ae317bdb
Sonu kumar kushwaha <51280276+flyingsonu122@users.noreply.github.com>
2021-01-05 12:30:17 +0530

    updated

--- random_question.js
+++ random_question.js
@@ -1,5 +1,5 @@
 const op = require('./app');
-// console.log(op);
+
 const readline =  require('readline');
 const rl = readline.createInterface({input : process.stdin, output : process.stdout});
 
@@ -7,38 +7,46 @@ let num1 = Math.floor((Math.random() * 10) + 1);
 let num2 = Math.floor((Math.random() * 10) + 1);
 let fun = [,op.plus(num1, num2), op.minus(num1, num2),op.product(num1, num2), op.division(num1, num2)];
 let operator = [,op.pl,op.mi,op.pr,op.di]
-// console.log(fun);
-// for(i=0;i<50;i++){
-//     console.log(fun[Math.floor((Math.random() * 4) + 1)]);
-// }
+
 let choice = Math.floor((Math.random() * 4) + 1);
 let f = fun[choice];
 let o = operator[choice];
 let answer = f;
 // console.log(answer);
-// console.log(o);
+let count = 0;
+
 
 rl.question(`What is ${ num1 } ${o} ${ num2 } ? \n`, (userInput) => {
     if(userInput.trim() == answer) {
+        console.log("Correct");
         rl.close();
     }
     else {
+        count++;
         rl.setPrompt('Incorrect response, Please try again! \n');
         rl.prompt();
         rl.on('line', (userInput) => {
             if(userInput.trim() == answer) {
+                console.log("Correct");
                 rl.close();
             }
-            else {
+            else if (count < 10){
+                count++;
                 rl.setPrompt(`Your answer of ${ userInput } is incorrect, Try again! \n`);
                 rl.prompt();
+                // console.log(count);
+            }
+            else {
+                console.log('\nYou lose !!! \n');
+                console.log(`The answer is ${ answer }  \n`);
+                rl.close();
             }
         });
     }
 });
 
 rl.on('close', () => {
-    console.log('Correct !!! \n');
+    console.log('Thank You !\nPlay again');
 });
 
 
